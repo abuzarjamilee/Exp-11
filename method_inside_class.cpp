69040a7bbd5d2bad0f8d6d952c92049f43784c31
abuzarjamilee <abuzarjamilee@gmail.com>
2024-10-23 01:25:03 +0530

    Add files via upload

---
+++ method_inside_class.cpp
@@ -0,0 +1,25 @@
+//Name:Abuzar Jamilee
+//PRN:23070123158
+//Aim:Implemention of Classes and Objects to define method inside class
+
+#include<iostream>
+using namespace std;
+
+class Student               
+{
+    public:
+    void myMethod()
+    {
+        cout<<"I am studying in 2nd year";
+    }
+};
+
+int main()
+{
+    Student s1;
+    s1.myMethod();
+}
+
+
+/*Output:
+I am studying in 2nd year*/
